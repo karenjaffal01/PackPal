62e7dfb5c869584e5c450963af9b2c06269ed6ee
karen jaffal <karenjaffal05.gmail.com>
2025-07-02 02:29:45 +0300

    Add project files.

---
+++ PackPal/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PackPal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
