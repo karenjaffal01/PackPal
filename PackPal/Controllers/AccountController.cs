a1184bb42224e943c1aae35c07a3a4dd41c7c779
karen jaffal <karenjaffal05.gmail.com>
2025-07-08 03:39:47 +0300

    Setup DB and viewModels for registration and authorization

---
+++ PackPal/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PackPal.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login(){  //controllers can return: view(usually.cshtml),redirect,JSON result,file or nothing
+            return View(); //since it returns it will look for view called Login
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult VerifyEmail()
+        {
+            return View();
+        }
+        public IActionResult ChangePassword()
+        {
+            return View();
+        }
+    }
+}
--- PackPal/ViewModels/VerifyEmailViewModel.cs
+++ PackPal/ViewModels/VerifyEmailViewModel.cs
@@ -5,19 +5,8 @@ namespace PackPal.ViewModels
 {
     public class VerifyEmailViewModel
     {
-        [Required(ErrorMessage = "Email is required")]
+        [Required(ErrorMessage = "Email is required.")]
         [EmailAddress]
         public string Email { get; set; }
-
-        [Required(ErrorMessage = "Password is required")]
-        [DataType(DataType.Password)]
-        [StringLength(40, MinimumLength = 8, ErrorMessage = "Password must be of length between 8 and 40")]
-        [Compare("ConfirmPassword", ErrorMessage = "Password and Confirmation does not match")]
-        public string NewPassword { get; set; }
-
-        [Required(ErrorMessage = "Password Confirmation is required")]
-        [DataType(DataType.Password)]
-        [DisplayName("Confirm New Password")]
-        public string ConfirmNewPassword { get; set; }
     }
 }
